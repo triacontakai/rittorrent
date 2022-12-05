b156f4e02b87e35707d3262f22200053caeac6db
Ryan El Kochta <git@relkochta.com>
2022-12-05 15:42:41 -0500

    http: little bit more

--- src/http.rs
+++ src/http.rs
@@ -7,6 +7,7 @@ use anyhow::{anyhow, Result};
 use url::{ParseError, Position, Url};
 
 const TIMEOUT: Duration = Duration::from_secs(5);
+const CRLF: &[u8] = b"\r\n";
 
 #[derive(Debug)]
 pub struct Response {
@@ -35,10 +36,17 @@ where
     // Next, let's try to connect to the remote
     let addrs = parsed_url.socket_addrs(|| None)?;
     let mut stream = TcpStream::connect(&*addrs)?;
-    stream.set_write_timeout(Some(TIMEOUT));
+    stream.set_write_timeout(Some(TIMEOUT))?;
 
     // Send the path
+    let path = &parsed_url[Position::BeforePath..Position::AfterQuery];
     stream.write(b"GET ")?;
+    stream.write(path.as_bytes())?;
+    stream.write(b" HTTP/1.1")?;
+    stream.write(CRLF)?;
+
+    // skip headers for now
+    stream.write(CRLF)?;
 
     // TODO: delete this
     Ok(Response {
@@ -56,6 +64,6 @@ mod tests {
     fn http_get_1() {
         let mut query = HashMap::new();
         query.insert("query1".to_owned(), "value1".to_owned());
-        super::http_get("http://google.com/some/path", &[("query1", "value1")]).unwrap();
+        super::http_get("http://128.8.126.63:21212/announce", &[("query1", "value1")]).unwrap();
     }
 }
